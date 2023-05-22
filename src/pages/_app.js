5402e0151fb6352ad85df3e0147325bf723c582c
Alexander Ilyashenko <83316201+Akargzom@users.noreply.github.com>
2023-05-22 22:21:53 +0300

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import { ApolloProvider } from '@apollo/client'
 import '../styles/globals.css'
 import { client } from '../apollo/apollo'
+import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps}) {
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
+      <Analytics />
     </ApolloProvider>
   )
 }
