b3f913914383b9ff3d8ad47e51e1d0f2c6cbe8cb
Alexander Ilyashenko <83316201+Akargzom@users.noreply.github.com>
2023-05-22 19:22:35 +0300

    css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Home from '../components/screens/home/home'
 import { GET_ALL_POSTS, GET_HOME, GET_MENU_FOOTER, GET_MENU_HEADER, GET_SERVER_POSTS, } from '../queries/get_queries'
 import { client } from '../apollo/apollo'
 import { Inter } from 'next/font/google'
-import Head from 'next/head'
 import Layout from '../components/layout/layout'
 const inter = Inter({ subsets: ['latin'] })
 export default function HomePage(props) {
