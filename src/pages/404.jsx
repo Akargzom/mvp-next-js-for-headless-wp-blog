4758a31c66394932351dd3dc92f42a98a90c9482
Alexander Ilyashenko <83316201+Akargzom@users.noreply.github.com>
2023-05-26 17:43:19 +0300

    404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,33 @@
 import Image from "next/image"
-import Layout from "../components/layout/layout"
-
- export default function Custom404() {
+import { GET_MENU_FOOTER, GET_MENU_HEADER } from '../queries/get_queries'
+import { client } from '../apollo/apollo'
+import Header from "../components/layout/header/header"
+import Footer from "../components/layout/Footer/footer"
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+ export default function Custom404(props) {
 return(
+        <div className={inter.className}>
+        <Header getHead={props?.getHead}/>
         <div className="container">
         <Image className="notFound" src='/404.png' alt="404" width={500} height={500}/>
         </div>
+        <Footer getFoot={props?.getFoot}/>
+        </div>
 )
 }
+export async function getStaticProps() {
+          const getHead = await client.query({
+            query: GET_MENU_HEADER
+          }),
+          getFoot = await client.query({
+            query: GET_MENU_FOOTER
+          })
+        return {
+          props: {
+            getHead,
+            getFoot
+          },
+          revalidate: 60
+        };
+      }
